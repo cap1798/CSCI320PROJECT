827bfb4e74a5da34e0a42adb27baf3d45f21bd11
ChrisPapaventsis <ChrisAlexPapav2006@gmail.com>
2025-10-15 13:31:46 -0400

    accessing the database

--- main.py
+++ main.py
@@ -1,10 +1,44 @@
-import tkinter as tk
+import psycopg # library for database
+from sshtunnel import SSHTunnelForwarder
+import tkinter as tk # library for graphics
+
+# credentials
+username = "YOUR_CS_USERNAME"
+password = "YOUR_CS_PASSWORD"
+dbName = "YOUR_DB_NAME"
+
+
+try:
+    with SSHTunnelForwarder(('starbug.cs.rit.edu', 22),
+                            ssh_username=username,
+                            ssh_password=password,
+                            remote_bind_address=('127.0.0.1', 5432)) as server:
+        server.start()
+        print("SSH tunnel established")
+        params = {
+            'dbname': dbName,
+            'user': username,
+            'password': password,
+            'host': 'localhost',
+            'port': server.local_bind_port
+        }
+
+
+        conn = psycopg.connect(**params)
+        curs = conn.cursor()
+        print("Database connection established")
+
+        #DB work here....
+
+        conn.close()
+except:
+    print("Connection failed")
 
 class MyApp(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         self.title("Video Games")
-        self.geometry("1024x600")
+        self.geometry("1024x600") # screen resolution
 
 root = MyApp()
 root.mainloop()
